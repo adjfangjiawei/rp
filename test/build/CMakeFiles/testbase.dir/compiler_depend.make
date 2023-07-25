d1428a99ffc295879060e97db0567a1bd37ef3dd
adj <3195727347@qq.com>
2023-07-25 13:07:42 +0800

    update .gitignore

--- test/build/CMakeFiles/testbase.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for testbase.
-# This may be replaced when dependencies are built.
--- test/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /hqzn/rp/test
-# Build directory: /hqzn/rp/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[testbase]=] "/hqzn/rp/test/build/testbase")
-set_tests_properties([=[testbase]=] PROPERTIES  _BACKTRACE_TRIPLES "/hqzn/rp/test/CMakeLists.txt;18;add_test;/hqzn/rp/test/CMakeLists.txt;0;")
