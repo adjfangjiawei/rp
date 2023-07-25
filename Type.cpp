9c8851ecbb9aa8fbdc2e10863c63c541347e2089
adj <3195727347@qq.com>
2023-07-25 09:35:50 +0800

    test:测试看能否正常提交

--- Type.cpp
+++ Type.cpp
@@ -1,6 +1,46 @@
-#include <string>
 #include <iostream>
+#include <string>
 
-int main(){
-    std::cout<<"hello world";
-}
+// 抽象类
+class ExprAST {
+public:
+  virtual void pprint(std::ostream &os) = 0;
+};
+
+// 变量语法树
+class VariableExprAST : public ExprAST {
+public:
+  virtual void pprint(std::ostream &os) = 0;
+};
+
+class InstantValueExprAST : public ExprAST {
+  virtual void pprint(std::ostream &os) = 0;
+};
+
+// 一元表达式
+class UnaryExprAST : public ExprAST {
+public:
+private:
+  char op;
+  ExprAST *expr;
+};
+
+// 整型变量
+class IntegerVariableExprAST : public VariableExprAST {
+public:
+  virtual void pprint(std::ostream &os) {}
+
+private:
+  int val;
+  std::string name;
+};
+
+// 整型立即数
+class IntegerInstantValueExprAST : public InstantValueExprAST {
+public:
+  virtual void pprint(std::ostream &os) {}
+
+private:
+  int val;
+};
+int main() { std::cout << "hello world"; }
